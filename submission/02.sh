a1887c8cde4e1ec3944c88e0b8ab26fbf7e54842
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2025-04-03 16:48:12 +0100

    final submit

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,6 @@
 # Create a native segwit address and get the public key from the address.
+SEGWIT_ADDRESS=$(bitcoin-cli -regtest getnewaddress "" "bech32")
+
+PUB_KEY=$(bitcoin-cli -regtest getaddressinfo $SEGWIT_ADDRESS | jq -r ".pubkey")
+
+echo $PUB_KEY
--- submission/06.sh
+++ submission/06.sh
@@ -1,2 +1,6 @@
 # What is the hash of this partially signed transaction?
-# transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
+transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
+
+PSBT_HASH=$(bitcoin-cli -regtest decodepsbt $transaction | jq -r '.tx | .hash' )
+
+echo $PSBT_HASH
